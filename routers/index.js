95dada1f973493c60b69cd438ed6148743138545
root <root@nodejs.asia-east1-a.c.tactile-carver-192423.internal>
2018-10-31 18:19:51 +0000

    2ed commit

---
+++ routers/index.js
@@ -0,0 +1,7 @@
+const nextRoutes = require('next-routes');
+const routes = module.exports = nextRoutes();
+
+routes.add('index', '/');
+//routes.add('works', '/works');
+routes.add('about', '/about');
+//routes.add('tac', '/works/tac');
--- server.js
+++ server.js
@@ -1,30 +1,19 @@
-const express = require('express')
-const next = require('next')
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const express = require('express');
+const routes = require('./routers');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handler = routes.getRequestHandler(app);
+const PORT = process.env.PORT || 80;
 
-app.prepare()
-.then(() => {
-  const server = express()
-/**
-  server.get('/p/:id', (req, res) => {
-    const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
-    app.render(req, res, actualPage, queryParams)
+app.prepare().then(() => {
+  const server = express();
+  server.get('*', (req, res) => handler(req, res));
+  server.use(handler).listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`)
   })
-**/
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(80, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost')
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+});
